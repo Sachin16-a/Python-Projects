7f533a68e6667d36d2ad46f0164a6a3d2b274925
Sachin16-a <68729194+Sachin16-a@users.noreply.github.com>
2022-12-09 19:38:59 +0530

    Add files via upload
    Japanese Kanji Learning

---
+++ Japanese Kanji Flashcards/main.py
@@ -0,0 +1,100 @@
+""" Program for Kanji Flashcards. """
+
+import random
+import tkinter as tk
+import pandas as pd
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/japanese_kanji.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    """Function to get the next flashcard"""
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Kanji", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Kanji"], fill="black")
+    canvas.itemconfig(card_bg, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    """Function to flip the card and show english meaning."""
+    canvas.itemconfig(
+        card_title,
+        text=[
+            "English",
+            "/",
+            "Onyomi(chinese origin)",
+            "/",
+            "Kunyomi(japanese origin)",
+        ],
+        font=("arial", 17, "bold", "italic"),
+        fill="white",
+    )
+    canvas.itemconfig(
+        card_word,
+        text=[
+            current_card["English Meaning"],
+            "/",
+            current_card["Onyomi(chinese origin)"],
+            "/",
+            current_card["Kunyomi(japanese origin)"],
+        ],
+        font=("arial", 20, "bold"),
+        fill="white",
+    )
+    canvas.itemconfig(card_bg, image=card_back_img)
+
+
+def is_known():
+    """Function to remove flashcards you've already learned."""
+    to_learn.remove(current_card)
+    words_to_learn = pd.DataFrame(to_learn)
+    words_to_learn.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+window = tk.Tk()
+window.title("Learn Kanji")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = tk.Canvas(width=800, height=526)
+card_front_img = tk.PhotoImage(file="images/card_front.png")
+card_back_img = tk.PhotoImage(file="images/card_back.png")
+card_bg = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(
+    400, 150, text="", font=("arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("arial", 40, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+
+
+cross_image = tk.PhotoImage(file="images/wrong.png")
+unknown_button = tk.Button(
+    image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(column=0, row=1)
+
+check_image = tk.PhotoImage(file="images/right.png")
+known_button = tk.Button(
+    image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(column=1, row=1)
+
+next_card()
+
+
+window.mainloop()
